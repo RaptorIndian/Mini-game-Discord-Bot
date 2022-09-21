8a87a3cd23eae61b8c61aa0872a7b60a44f165bb
Layla-AC <91022714+Layla-AC@users.noreply.github.com>
2022-09-21 19:15:13 -0400

    Update main-test.py
    Add documentation to main-test.py, still awaiting integration with discord in main.py

--- main-test.py
+++ main-test.py
@@ -6,6 +6,8 @@ sys.path.append('classes')
 from classes import game
 Game = game.Game
 
+# Spawn a new Game object
 instance = Game(("Bot 1", "Bot 2"), (0, 0), 1, True)
 
+# Start the game instance- can be deferred until the discord bot is ready.
 instance.start()
