ba8b4bd760ee1ec8ac1e53782c6578314f06e912
Layla-AC <91022714+Layla-AC@users.noreply.github.com>
2022-09-21 19:16:10 -0400

    Update util.py
    Add documentation to util.py

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+# Take an Array and join it into a String - Only useful in the pre-discord implementation?
 def stringify(delim, arr):
     output = ""
     length = len(arr)
@@ -5,5 +6,6 @@ def stringify(delim, arr):
         output += arr[i] + ("", delim) [i < length - 1]
     return output
 
+# Check for NaN- unused atm.
 def isNaN(value):
     return int(value) != int(value)
